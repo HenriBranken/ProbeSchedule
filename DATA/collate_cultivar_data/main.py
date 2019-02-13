63bb0c000f3af25802999406efaa4869c01063b8
HenriBranken <henri.branken777@gmail.com>
2019-02-13 18:59:16 +0200

    refined 'working with time series data'

--- DATA/collate_cultivar_data/main.py
+++ DATA/collate_cultivar_data/main.py
@@ -4,25 +4,36 @@ from datetime import timedelta
 import pandas as pd
 import matplotlib as matplotlib
 import matplotlib.pyplot as plt
-from cleaning_operations import *
+# from cleaning_operations import *
+import cleaning_operations
+
+
+def load_probe_data(probe_name):
+    dataframe = pd.read_excel("../Golden_Delicious_daily_data.xlsx", sheet_name=probe_name,
+                              index_col=0, parse_dates=True)
+    refined_columns = []
+    for c in dataframe.columns:
+        refined_columns.append(c.lstrip())
+    dataframe.columns = refined_columns
+    return dataframe
+
+
+def initialize_flagging_columns(dataframe):
+    dataframe["binary_value"] = 0.0
+    dataframe["description"] = str()
+    return dataframe
 
 
 probe_ids = ["P-370", "P-371", "P-372", "P-384", "P-391", "P-392", "P-891"]
 
-# TODO: place all the code below inside a for-loop; we need to iterate over all the probes.
+# TODO: encase all the code below inside a for-loop; we need to iterate over all the probes present in the list.
 
 probe_id = "P-371"
 
-df = pd.read_excel("../Golden_Delicious_daily_data.xlsx", sheet_name=probe_id, index_col=0, parse_dates=True)
-refined_columns = []
-for c in df.columns:
-    refined_columns.append(c.lstrip())
-df.columns = refined_columns
+df = load_probe_data(probe_name=probe_id)
 
-# Add `binary_value` and `description` columns to the probe's DataFrame
-# These columns will help to keep track of flagging operations
-df["binary_value"] = 0.0
-df["description"] = str()  # an empty string
+df = initialize_flagging_columns(dataframe=df)
 
 # Cleaning operation 1: drop redundant columns
-drop_redundant_columns(df)
+cleaning_operations.drop_redundant_columns(df=df)
+print(df.columns)
